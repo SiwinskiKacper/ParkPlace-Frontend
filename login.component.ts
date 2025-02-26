7c4e1139424511138e3e2e1883d4993fec9305ca
aleksandermejergl <aleksander.mejer@globallogic.com>
2025-02-26 23:49:06 +0100

    Add files via upload

---
+++ login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  login() {
+    console.log('Login próba:', this.username, this.password);
+  }
+}
