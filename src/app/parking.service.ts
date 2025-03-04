4a6cdf43144ddf32237f91b3ff5f8c09f53449a2
SiwińskiKacperGL <kacper.siwinski@globallogic.com>
2025-03-04 14:06:09 +0100

    modified:   src/app/parking.service.ts

--- src/app/parking.service.ts
+++ src/app/parking.service.ts
@@ -11,14 +11,6 @@ export class ParkingService {
 
   constructor(private http: HttpClient) {}
 
-  protected parkingSpots: ParkingSpot[] = [
-    {id: 0, zone: 'A', floor: 1, number: 1, available: false},
-    {id: 1, zone: 'A', floor: 1, number: 2, available: false},
-    {id: 2, zone: 'A', floor: 2, number: 1, available: false},
-    {id: 3, zone: 'B', floor: 1, number: 1, available: true},
-    {id: 4, zone: 'B', floor: 1, number: 2, available: true},
-  ];
-
   getAllParkingSpots(): Observable<ParkingSpot[]> {
     return this.http.get<ParkingSpot[]>(`${this.base_url}/api/Parking/available-spots`);
   }
