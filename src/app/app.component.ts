95435b5dc9d8d4825116336500220e82181215f8
Damian866 <damian.kucharski@globallogic.com>
2025-03-04 13:21:58 +0100

    made a registration view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { DisplayParkSpotsComponent } from './display-park-spots/display-park-spots.component';
+import { RegisterPanelComponent } from './register-panel/register-panel.component';
+import { DOCUMENT } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ParkingSpotsDataService } from './parking-spots-data.service';
 
 @Component({
   selector: 'app-root',
-  imports: [DisplayParkSpotsComponent],
+  imports: [DisplayParkSpotsComponent, RegisterPanelComponent, RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
-export class AppComponent {}
+export class AppComponent {
+   
+}
