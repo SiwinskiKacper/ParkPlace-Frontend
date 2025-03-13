9dd5040b218c69cb45723a20ba81d5bed35e0d12
aleksandermejergl <aleksander.mejer@globallogic.com>
2025-03-13 19:12:04 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { RegisterPanelComponent } from './register-panel/register-panel.component';
 import { ReservationDashboardComponent } from './reservation-dashboard/reservation-dashboard.component';
 import { ReservationPanelComponent } from './reservation-panel/reservation-panel.component';
+import { LoginComponent } from './login/login.component';
 import { ReservationHistoryPanelComponent } from './reservation-history-panel/reservation-history-panel.component';
 
 export const routes: Routes = [
   { path: '', component: RegisterPanelComponent },
   { path: 'dashboard', component: ReservationDashboardComponent },
   { path: 'reservation', component: ReservationPanelComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'reservation-history', component: ReservationHistoryPanelComponent },
 ];
